a7a94786a9255b4205dc056f13f8ec18e6f10a0e
Roshan Kumar <rsahu7989@gmail.com>
2024-05-07 02:04:18 +0530

    Add custom colors and font family to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{jsx,js,ts,,tsx}'],
+    darkMode: "class",
+    content: ['./index.html', './src/**/*.{jsx,js,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "primary": 'var(--primary-color)',
+                "secondary": 'var(--secondary-color)',
+
+            },
+            fontFamily: {
+                'sans': ['Rubik', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 }
