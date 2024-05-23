1fcdc60d0949842bde36632c9cc2903640f06df7
Roshan Kumar <rsahu7989@gmail.com>
2024-05-24 02:14:51 +0530

    refactor: Update animation times in App.css and add new skills to skillsData.js

--- src/components/Skills/skillsData.js
+++ src/components/Skills/skillsData.js
@@ -1,29 +1,35 @@
 // skillsData.js
 
 export const skills = [
+    // Backend
     { name: 'Node.js', color: '#339933b0' },
-    { name: 'JavaScript', color: '#F7DF1Eb0' },
-    { name: 'Bootstrap', color: '#7952B3b0' },
-    { name: 'HTML', color: '#E34F26b0' },
     { name: 'MongoDB', color: '#47A248b0' },
-    { name: 'CSS', color: '#1572B6b0' },
-    { name: 'Redux', color: '#764ABCb0' },
-    { name: 'Git', color: '#F05032b0' },
     { name: 'aws', color: '#FF9900b0' },
     { name: 'Docker', color: '#2496EDb0' },
+    // Frontend
+    { name: 'JavaScript', color: '#F7DF1Eb0' },
+    { name: 'HTML', color: '#E34F26b0' },
+    { name: 'CSS', color: '#1572B6b0' },
+    { name: 'Bootstrap', color: '#7952B3b0' },
+    { name: 'Redux', color: '#764ABCb0' },
+    // Tools
+    { name: 'Git', color: '#F05032b0' },
 ];
 
 export const skills2 = [
+    // Backend
+    { name: 'Node.js', color: '#339933b0' },
+    { name: 'Express', color: '#f6f5f2b0' },
+    { name: 'MongoDB', color: '#47A248b0' },
+    { name: 'Mysql', color: '#4479A1b0' },
+    // Frontend
     { name: 'JavaScript', color: '#F7DF1Eb0' },
-    { name: 'GitHub', color: '#f6f5f2b0' },
     { name: 'React', color: '#61DAFBb0' },
     { name: 'Redux', color: '#764ABCb0' },
-    { name: 'Node.js', color: '#339933b0' },
     { name: 'HTML', color: '#E34F26b0' },
-    { name: 'Express', color: '#f6f5f2b0' },
     { name: 'Bootstrap', color: '#7952B3b0' },
-    { name: 'MongoDB', color: '#47A248b0' },
-    { name: 'Figma', color: '#F24E1Eb0' },
-    { name: 'Mysql', color: '#4479A1b0' },
     { name: 'D3.js', color: '#F9A03Cb0' },
+    // Tools
+    { name: 'GitHub', color: '#f6f5f2b0' },
+    { name: 'Figma', color: '#F24E1Eb0' },
 ];
