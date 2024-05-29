f1668e93f1c21ba5fa2a5346fdcb3a73dd472068
Roshan Kumar <rsahu7989@gmail.com>
2024-05-30 01:54:59 +0530

    refactor: Improve hover effect for menu items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import { useRef, useState } from 'react'
 import Menu01Icon from './icons/Menu01Icon'
+import '../styles/buttonHoverEffect.css'
 
 const Navbar = ({ activeSection }) => {
     const menuRef = useRef(null)
@@ -24,37 +25,37 @@ const Navbar = ({ activeSection }) => {
                         <div
                             ref={menuRef}
                             id='menu'
-                            className={`menu ${menustate ? 'menu-open' : ''}   h-full  w-full overflow-hidden  md:block  md:w-auto `}>
-                            <ul className='*:text-shadow-custom flex h-full flex-col items-start justify-center gap-4 *:text-lg *:font-semibold   *:text-primary hover:*:text-secondary  dark:*:text-white dark:hover:*:text-secondary md:flex md:flex-row md:items-center md:gap-0 md:space-x-6'>
-                                <li>
+                            className={`menu ${menustate ? 'menu-open' : ''}    h-full  w-full overflow-hidden  md:block  md:w-auto `}>
+                            <ul className='*:text-shadow-custom  flex h-full flex-col items-start justify-center *:cursor-pointer    *:p-2 *:px-3 *:text-lg *:font-semibold *:text-primary   hover:*:text-secondary  dark:*:text-white dark:hover:*:text-secondary md:flex md:flex-row md:items-center md:gap-0 md:space-x-6'>
+                                <li className='hover-effect'>
                                     <a
                                         href='#home'
                                         className={`${activeSection == 'home' ? 'text-secondary' : ''}`}>
                                         Home
                                     </a>
                                 </li>
-                                <li>
+                                <li className='hover-effect'>
                                     <a
                                         href='#project'
                                         className={`${activeSection == 'project' ? 'text-secondary' : ''}`}>
                                         Projects
                                     </a>
                                 </li>
-                                <li>
+                                <li className='hover-effect'>
                                     <a
                                         href='#skills'
                                         className={`${activeSection == 'skills' ? 'text-secondary' : ''}`}>
                                         Skills
                                     </a>
                                 </li>
-                                {/* <li>
+                                {/* <li className='hover-effect'>
                                     <a
                                         href='#about'
                                         className={`${activeSection == 'about' ? 'text-secondary' : ''}`}>
                                         About
                                     </a>
                                 </li> */}
-                                <li>
+                                <li className='hover-effect'>
                                     <a
                                         href='#contact'
                                         className={`${activeSection == 'contact' ? 'text-secondary' : ''}`}>
@@ -65,7 +66,7 @@ const Navbar = ({ activeSection }) => {
                         </div>
                         <div
                             onClick={toggleMenu}
-                            className={`group transition-colors ${menustate ? 'bg-[#ee5f3850]' : 'bg-transparent'} absolute right-6 top-[15px] cursor-pointer rounded-md  p-2 md:hover:bg-[#ee5f3850]`}>
+                            className={`group transition-colors ${menustate ? 'bg-[#ee5f3850]' : 'bg-transparent'} absolute right-6 top-[15px] block cursor-pointer rounded-md p-2  md:hidden md:hover:bg-[#ee5f3850]`}>
                             <Menu01Icon
                                 className={` ${menustate ? 'text-secondary' : 'text-black dark:text-white'} transition-colors  md:hidden`}
                             />
